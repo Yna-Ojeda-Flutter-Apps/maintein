adf81912a4c7ef421dc0799bf17cc2f644c836f9
Alyssa Jubay <beautylove.aj@gmail.com>
2020-02-29 21:48:32 +0800

    change gif to video

--- lib/screens/breathing/breathing_exercise.dart
+++ lib/screens/breathing/breathing_exercise.dart
@@ -1,17 +1,55 @@
 import 'package:maintein/widgets/bottomnavbar.dart';
 import 'package:flutter/material.dart';
-
-class BreathingExercise extends StatelessWidget {
+import 'package:video_player/video_player.dart';
+class BreathingExercise extends StatefulWidget {
   static const routeName = '/breathe';
+
+  @override
+  State<StatefulWidget> createState() {
+    return BreathingState();
+  }
+}
+
+class BreathingState extends State<BreathingExercise> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState(){
+    _controller = VideoPlayerController.asset('lib/assets/videos/BreathingGuide_v2.mp4');
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.setVolume(1.0);
+    _controller.play();
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: research on military breathing exercise
     return Scaffold(
-      backgroundColor: Color(0xfffbfbfb),
       body: Center(
-        child: Image(
-          image: AssetImage('lib/assets/images/breathing.gif'),
-          
+        child: FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot){
+            if (snapshot.connectionState == ConnectionState.done){
+              return Center(
+                child: AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
+              );
+            } else {
+              return Center(
+                child:CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
       bottomNavigationBar: BottomNavBar(),
